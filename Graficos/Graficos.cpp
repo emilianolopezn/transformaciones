32dea75e540874da8994b2f8580d9f105148cfbd
alumno <alumno@D3AMAESTRO>
2018-01-29 10:41:52 -0700

    Add project files.

---
+++ Graficos/Graficos.cpp
@@ -0,0 +1,115 @@
+// Graficos.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define GLEW_STATIC
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+
+#include <iostream>
+using namespace std;
+
+GLfloat red, green, blue;
+
+void actualizar() { 
+	//Aquí esta bien para cambiar los valores
+	//De las variables de mi programa!
+	
+	/*red += 0.001;
+	green += 0.002;
+	blue += 0.003;
+
+	if (red > 1) red = 0;
+	if (green > 1) green = 0;
+	if (blue > 1) blue = 0;*/
+
+}
+
+void dibujar() {
+	glBegin(GL_TRIANGLES); //Inicia la rutina con un modo de dibujo
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(1.0, 0.0f, 0.0f);
+
+	glEnd();//Finaliza la rutina
+}
+
+
+int main()
+{
+	//Declaración de ventana
+	GLFWwindow *window;
+	//Propiedades de la ventana
+	GLfloat ancho = 1024;
+	GLfloat alto = 768;
+
+	//Inicialización de GLFW
+	if (!glfwInit()) {
+		//Si no se inició bien, terminar la ejecución
+		exit(EXIT_FAILURE);
+	}
+
+	//Inicializar la ventana
+	window = glfwCreateWindow(ancho, alto, "Graficos", NULL, NULL);
+	//Verficar si se creó bien la ventana
+	if (!window) {
+		//Cerrar todos los procesos de GLFW
+		glfwTerminate();
+		//Termina ejecución
+		exit(EXIT_FAILURE);
+	}
+
+	//Establecer "window" como contexto
+	glfwMakeContextCurrent(window);
+
+	//Se trae las funciones de OpenGL Moderno
+	glewExperimental = true;
+	//Inicializar GLEW
+	GLenum glewError = glewInit();
+	//Verificar que GLEW se inicializó bien
+	if (glewError != GLEW_OK) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+
+	const GLubyte *version = glGetString(GL_VERSION);
+	cout << "Version de OpenGL: " << version << endl;
+
+
+	red = green = blue = 0.0f;
+
+	//Ciclo de dibujo
+	while (!glfwWindowShouldClose(window)) {
+		//Establecer el area de render (viewport)
+		glViewport(0, 0, ancho, alto);
+		//Establecer el color con el que se limpia la pantalla
+		glClearColor(red, green, blue, 1);
+		//Limpiar la pantalla
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//rutina de dibujo
+		actualizar();
+		dibujar();
+
+		//Intercambio de buffers
+		glfwSwapBuffers(window);
+		//Buscar señales de entrada
+		glfwPollEvents();
+
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+    return 0;
+}
+
